8556bf64a0173eee9035493a721d23f3b0aa56e5
Lovish Dogra <lovishdogra7@gmail.com>
2016-03-22 01:14:33 +0530

    Final commit
    Implementation of swipe & shake gesture

--- Shakes & Swipes/ViewController.swift
+++ Shakes & Swipes/ViewController.swift
@@ -10,14 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var actionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view.addGestureRecognizer(swipeUp)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.MotionShake{
+            actionLabel.text = "Device Shaked"
+        }
+    }
+    
+    func swiped(gesture : UIGestureRecognizer){
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer{
+            switch swipeGesture.direction{
+            case UISwipeGestureRecognizerDirection.Right:
+                actionLabel.text = "Swiped Right"
+            case UISwipeGestureRecognizerDirection.Up:
+                actionLabel.text = "Swiped Up"
+            default:
+                print("Not able to recognize gesture")
+            }
+        }
     }
 
 
